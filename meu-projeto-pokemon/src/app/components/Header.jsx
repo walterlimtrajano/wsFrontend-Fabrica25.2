8a6bb6b1bdd6d645cbd621f677cc74daa270dbf2
walterlimtrajano <128225888+walterlimtrajano@users.noreply.github.com>
2025-09-13 21:48:27 -0300

    Mudando os textos do Header e do Footer.

--- meu-projeto-pokemon/src/app/components/Header.jsx
+++ meu-projeto-pokemon/src/app/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white p-4 text-center font-bold text-xl">
-      Meu Projeto Pokémon
+       Projeto Pokémon de Walterlim
     </header>
   )
 }
